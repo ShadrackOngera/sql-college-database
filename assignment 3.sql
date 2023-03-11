be3c1889329d7b003ad02c61347cf291ad4e78e8
shadrackongera <shadrack.sme@gmail.com>
2023-03-11 12:22:24 +0300

    adding data to tables

--- assignment 3.sql
+++ assignment 3.sql
@@ -6,14 +6,15 @@ CREATE TABLE collections ( collection_id INT PRIMARY KEY AUTO_INCREMENT, name VA
 CREATE TABLE categories ( category_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30) NOT NULL);
 CREATE TABLE libraryMaterials ( material_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30) NOT NULL), length VARCHAR(30);
 CREATE TABLE patronsLoans ( patrons_loan_id INT PRIMARY KEY AUTO_INCREMENT, loaned BOOLEAN NOT NULL);
-CREATE TABLE users ( user_id INT PRIMARY KEY AUTO_INCREMENT, first_name VARCHAR(40) NOT NULL, last_name VARCHAR(40) NOT NULL,);
+CREATE TABLE users ( user_id INT PRIMARY KEY AUTO_INCREMENT, first_name VARCHAR(40) NOT NULL, last_name VARCHAR(40) NOT NULL, address VARCHAR(255), city VARCHAR(20), state VARCHAR(20), zip VARCHAR(20));
+CREATE TABLE loanedItems ( loan_id INT PRIMARY KEY AUTO_INCREMENT, dateLoaned VARCHAR(50));
 
 
-INSERT INTO patrons VALUE (1, "Jane", "Doe", "27th Avenue", "Boston", "PA", "87654", "23E7N");
-INSERT INTO patrons VALUE (2, "James", "johnson", "27th circle street avenue", "Manchester", "PA", "23456", "23W7N");
-INSERT INTO patrons VALUE (3, "elvin", "Eriksen", "30th Street", "Boston", "WA", "23456", "13Q7N");
-INSERT INTO patrons VALUE (4, "Brian", "Doe", "Mehems Highway Avenue", "Boston", "LA", "12345", "23E7Z");
-INSERT INTO patrons VALUE (5, "John", "Zed", "27th Circle", "Boston", "WI", "2399", "93E0R");
+INSERT INTO patrons VALUE (1, "Jane", "Doe", "27th Avenue", "Boston", "PA", "87654");
+INSERT INTO patrons VALUE (2, "James", "johnson", "27th circle street avenue", "Manchester", "PA", "23456");
+INSERT INTO patrons VALUE (3, "elvin", "Eriksen", "30th Street", "Boston", "WA", "23456");
+INSERT INTO patrons VALUE (4, "Brian", "Doe", "Mehems Highway Avenue", "Boston", "LA", "12345");
+INSERT INTO patrons VALUE (5, "John", "Zed", "27th Circle", "Boston", "WI", "2399");
 
 INSERT INTO collections VALUE (1, "Books");
 INSERT INTO collections VALUE (2, "Magazines");
@@ -38,6 +39,23 @@ INSERT INTO patronsLoans VALUE (2, 1);
 INSERT INTO patronsLoans VALUE (3, 0);
 INSERT INTO patronsLoans VALUE (4, 1);
 INSERT INTO patronsLoans VALUE (5, 0);
+
+INSERT INTO loanedItems VALUE (1, "12/02/2023");
+INSERT INTO loanedItems VALUE (2, "22/02/2023");
+INSERT INTO loanedItems VALUE (3, "2/03/2023");
+INSERT INTO loanedItems VALUE (4, "13/01/2023");
+INSERT INTO loanedItems VALUE (5, "4/01/2023");
+INSERT INTO loanedItems VALUE (6, "2/02/2023");
+
+INSERT INTO users VALUE (1, "Melvin", "Peter", "jane street Avenue", "Compton", "LA", "87654");
+INSERT INTO users VALUE (2, "Steward", "Drary", "10th street Avenue", "Boston", "WI", "87654");
+INSERT INTO users VALUE (3, "Victor", "Doe", "13th street Avenue", "long beach", "LA", "87654");
+INSERT INTO users VALUE (4, "Calvin", "Mary", "jane street Avenue", "Malibu", "LA", "87654");
+INSERT INTO users VALUE (5, "Johnson", "Doe", "jane street Avenue", "Inglewood", "LA", "87654");
+
+ALTER TABLE categories ADD column CollectionID  INT;
+ALTER TABLE categories ADD CONSTRAINT collection_fk_categories FOREIGN KEY (CollectionID) REFERENCES collections(collection_id);
+UPDATE categories SET CollectionID = 1 WHERE category_id = 1;
 
 
 
