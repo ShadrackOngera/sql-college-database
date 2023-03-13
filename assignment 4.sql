aa0eafb5c268890bfb95443c672367c399133699
shadrackongera <shadrack.sme@gmail.com>
2023-03-14 00:57:57 +0300

    q1 code and screenshot

--- assignment 4.sql
+++ assignment 4.sql
@@ -4,15 +4,9 @@ USE tp0010;
 select * from 148students.states;
 
 
-SELECT r.name AS region_name, SUM(s.population) AS total_population
-FROM region r
-JOIN states s ON r.id = s.region_id
-GROUP BY r.id
+SELECT region, SUM(population) AS total_population
+FROM 148students.states
+GROUP BY region
 ORDER BY total_population DESC;
 
 
-SELECT r.name AS region_name, SUM(s.population) AS total_population
-FROM 148students.states r
-JOIN 148students.states s ON r.id = s.region_id
-GROUP BY r.id
-ORDER BY total_population DESC;
