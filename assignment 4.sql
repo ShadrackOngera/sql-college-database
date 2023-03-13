d9b337f42e171ce4d27c3906d14efa5a7d66f019
shadrackongera <shadrack.sme@gmail.com>
2023-03-14 01:03:59 +0300

    question 2 code and screenshot

--- assignment 4.sql
+++ assignment 4.sql
@@ -3,10 +3,18 @@ USE tp0010;
 
 select * from 148students.states;
 
-
+-- Question One code
 SELECT region, SUM(population) AS total_population
 FROM 148students.states
 GROUP BY region
 ORDER BY total_population DESC;
+
+
+-- Question 2 code
+SELECT region, MIN(year) AS first_state_established
+FROM 148students.states
+GROUP BY region
+ORDER BY first_state_established ASC;
+
 
 
